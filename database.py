3ec97d8f06dd55a3de5b22d1da8260d62f911a37
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-07-18 02:27:26 +0300

    Fix wrong url for SQLALCHEMY_DATABASE_URL

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite3:///./library.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./library.db"
 
 
 engine = create_engine(
