91d20cf05989bddd139d1741e162611502a410c7
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-07-18 12:18:40 +0300

    Add crud function create_book

--- crud.py
+++ crud.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 
 import models
+import schemas
 
 
 def get_author_by_id(db: Session, pk: int) -> models.Author | None:
@@ -9,3 +10,13 @@ def get_author_by_id(db: Session, pk: int) -> models.Author | None:
 
 def get_book_list(db: Session) -> list[models.Book]:
     return db.query(models.Book).all()
+
+
+def create_book(db: Session, data: schemas.BookCreateModel) -> models.Book:
+    book = models.Book(**data.model_dump())
+
+    db.add(book)
+    db.commit()
+    db.refresh(book)
+
+    return book
