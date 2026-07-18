1e94265757843a5b0735d7a6c96be594a078714e
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-07-18 12:21:49 +0300

    Add function generator get_db

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+from typing import Generator
+
 from fastapi import FastAPI
+
+from database import SessionLocal
+
+
+def get_db() -> Generator:
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 app = FastAPI()
