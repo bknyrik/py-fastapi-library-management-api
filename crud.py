83469d861bde2ecaa9f97cc4a72aa2ba6deff0e4
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-07-18 12:17:47 +0300

    Add function get_book_list

--- crud.py
+++ crud.py
@@ -5,3 +5,7 @@ import models
 
 def get_author_by_id(db: Session, pk: int) -> models.Author | None:
     return db.query(models.Author).get(ident=pk)
+
+
+def get_book_list(db: Session) -> list[models.Book]:
+    return db.query(models.Book).all()
