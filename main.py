1bedc3cac5719bcee1e6600b7bb6746b557680d4
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2026-07-18 12:32:34 +0300

    Add view write_book

--- main.py
+++ main.py
@@ -24,3 +24,11 @@ app = FastAPI()
 @app.get("/books/", response_model=list[schemas.BookModel])
 def get_all_books(db: Session = Depends(get_db)) -> list[models.Book]:
     return crud.get_book_list(db)
+
+
+@app.post("/books/", response_model=schemas.BookModel)
+def write_book(
+    data: schemas.BookCreateModel,
+    db: Session = Depends(get_db)
+) -> models.Book:
+    return crud.create_book(db=db, data=data)
